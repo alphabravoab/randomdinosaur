43d6b22a525b3ff2609cf2570e9a2bd45db7bbbe
Arjen <alphabravoab@gmail.com>
2020-09-06 22:10:35 +0200

    last of today

--- src/components/card/Lived.js
+++ src/components/card/Lived.js
@@ -1,7 +1,34 @@
 import React from 'react'
+import './card.css'
 
 const Lived = ({ from, to, era }) => {
-    return <div> Lived from {from} million years ago to {to} million years ago in the {era} era</div>
+    return (
+    <div> 
+        Lived from {from} million years ago to {to} million years ago in the {era} era
+        <div className="eras">
+            <div className="era">
+                <div>Triasic</div>
+                <div className="era">
+                <div className="EarlyTriasic eraDivider" />
+                <div className="eraDivider" />
+                <div className="eraDivider" />
+                </div>
+            </div>
+            <div className="era">
+            <div>Jurrasic</div>
+                <div className="earlyJurasic eraDivider" />
+                <div className="midJurrasic eraDivider" />
+                <div className="lateJurrasic eraDivider" />
+            </div>
+            <div className="cretaceous era">
+            <div>Cretaceous </div>
+                <div className="earlyCretaceous eraDivider"></div>
+                <div className="midCretaceous eraDivider" />
+                <div className="lateCretaceous eraDivider" />
+            </div>
+        </div>
+    </div>
+    )
 }
 
 export default Lived
--- src/components/card/Title.js
+++ src/components/card/Title.js
@@ -2,9 +2,12 @@ import React from 'react'
 import "./card.css"
 
 const Title = ({ name, pronunciation, meaning }) => {
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+      }
     return (
         <div className="head"> 
-            <h1>Name: {name} </h1>
+            <h1>Name: {capitalizeFirstLetter(name)} </h1>
             <div>pronunciation: {pronunciation}</div>
             <div>Meaning: {meaning}</div>
         </div>
