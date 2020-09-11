ee0e44820d2500f6d7396b9c5c6bc8580b0fde0a
Arjen <alphabravoab@gmail.com>
2020-09-11 11:13:02 +0200

    media adds

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -2,7 +2,17 @@ import React from 'react'
 import './banner.css'
 
 const Banner = () => {
-    return <div className="banner"> Banner goes here </div>
+    const addBanner = () => {
+        try {
+            window._mNHandle.queue.push(function (){
+                window._mNDetails.loadTag("844619673", "160x600", "844619673");
+            });
+        }
+        catch (error) {}
+    }
+    addBanner()
+    return <div className="banner"> <div id="844619673">
+</div></div>
 }
 
 export default Banner
